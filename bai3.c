511b7ee28cbcac738b99a554efcf3ec3c3619200
ivanovic9595 <38435396+ivanovic9595@users.noreply.github.com>
2018-06-02 04:01:43 +0700

    Update bai3.c

--- bai3.c
+++ bai3.c
@@ -13,7 +13,7 @@ else if(m>1000 && m<=30000){
 printf("tien cuoc la:%d VND",10000+(((m-1000)*1500)/200));
 }
 else if(m>30000){
-printf("tien cuoc la:%d",227500+((m-30000)*800));
+printf("tien cuoc la:%d",227500+((m-30000)*8));
 }
 }
 
